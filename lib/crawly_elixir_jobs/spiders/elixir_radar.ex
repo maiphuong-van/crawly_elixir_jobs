d6d8cb839b85365118e6f2a46fcd8df4ba6177f1
Phuong Van <phuong.van@erlang-solutions.com>
2020-08-25 17:51:03 +0200

    Crawl and extract data from elixir job

---
+++ lib/crawly_elixir_jobs/spiders/elixir_radar.ex
@@ -0,0 +1,65 @@
+defmodule ElixirRadar do
+  use Crawly.Spider
+
+  @impl Crawly.Spider
+  def base_url(), do: "https://elixir-radar.com"
+
+  @impl Crawly.Spider
+  def init() do
+    [
+      start_urls: [
+        "https://elixir-radar.com/jobs"
+      ]
+    ]
+  end
+
+  @impl Crawly.Spider
+  def parse_item(response) do
+    {:ok, document} = Floki.parse_document(response.body)
+
+    urls =
+      document
+      |> Floki.find("a.pagination__button")
+      |> Floki.attribute("href")
+
+    requests =
+      urls
+      |> Enum.uniq()
+      |> Enum.map(&build_absolute_url/1)
+      |> Enum.map(&Crawly.Utils.request_from_url/1)
+
+    jobs = document |> Floki.find(".job-board-job-details")
+
+    items = jobs |> Enum.map(&create_item/1)
+
+    %Crawly.ParsedItem{:items => items, :requests => requests}
+  end
+
+  defp build_absolute_url(url), do: URI.merge(base_url(), url) |> to_string()
+
+  defp create_item(job) do
+    title =
+      job
+      |> Floki.find(".job-board-job-title")
+      |> parse()
+
+    description =
+      job
+      |> Floki.find(".job-board-job-description")
+      |> parse()
+
+    location =
+      job
+      |> Floki.find(".job-board-job-location")
+      |> parse()
+
+    %{title: title, description: description, location: location}
+  end
+
+  defp parse(text) do
+    text
+    |> Floki.text()
+    |> String.replace(~r/\s+/, " ")
+    |> String.trim()
+  end
+end
