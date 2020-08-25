102bcca25752e39705465bba0ef61cfe22cb2b45
Phuong Van <phuong.van@erlang-solutions.com>
2020-08-25 14:47:43 +0200

    New elixir project

---
+++ lib/crawly_elixir_jobs.ex
@@ -0,0 +1,18 @@
+defmodule CrawlyElixirJobs do
+  @moduledoc """
+  Documentation for `CrawlyElixirJobs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CrawlyElixirJobs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/crawly_elixir_jobs_test.exs
@@ -0,0 +1,8 @@
+defmodule CrawlyElixirJobsTest do
+  use ExUnit.Case
+  doctest CrawlyElixirJobs
+
+  test "greets the world" do
+    assert CrawlyElixirJobs.hello() == :world
+  end
+end
